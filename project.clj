0ad1320f73dfc29578d0304e74b6d0eaeca75c16
Tim McCormack <cortex@brainonfire.net>
2011-12-10 14:51:42 -0500

    Use a hacked Enlive instead of sed for ensuring HTML validation.

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject seqs "0.0.1"
   :description "Explaining seqs vs. colls"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [enlive "1.0.0"]]
+                 [org.timmc/enlive "1.2.0-alpha1"]]
   :main seqs.core)
