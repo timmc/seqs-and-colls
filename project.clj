335a1dab18fe1bc28c1a690b791b97ff8a4e92f1
Tim McCormack <cortex@brainonfire.net>
2012-03-01 18:15:22 -0500

    Add repo URL to project

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject seqs "0.0.1"
   :description "Explaining seqs vs. colls"
+  :url "https://github.com/timmc/seqs-and-colls"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.timmc/enlive "1.2.0-alpha1"]]
   :main seqs.core)
